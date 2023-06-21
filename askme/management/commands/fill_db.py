8c7d7807d95694cf68ea5b6bfb582924d1ea6d24
Gallaann <reindzer26rus@gmail.com>
2023-06-21 19:52:24 +0300

    fixed fill option

--- askme/management/commands/fill_db.py
+++ askme/management/commands/fill_db.py
@@ -6,24 +6,27 @@ import random
 
 
 class Command(BaseCommand):
-    GENERATION_ORDER = 10
+
+    def add_arguments(self, parser):
+        parser.add_argument('ratio', type=int)
 
     def handle(self, *args, **options):
-        self.generate_user_and_profile()
+        ratio = options['ratio']
+        self.generate_user_and_profile(ratio)
         profiles = Profile.objects.all()
-        self.generate_tags()
+        self.generate_tags(ratio)
         tags = Tags.objects.all()
-        self.generate_questions(profiles)
+        self.generate_questions(profiles, ratio)
         questions = Questions.objects.all()
-        self.generate_answers(profiles, questions)
+        self.generate_answers(profiles, questions, ratio)
         answers = Answers.objects.all()
-        self.generate_question_likes(profiles, questions)
-        self.generate_answer_likes(profiles, answers)
+        self.generate_question_likes(profiles, questions, ratio)
+        self.generate_answer_likes(profiles, answers, ratio)
         for question in questions:
             for i in range(3):
                 question.tags.add(random.choice(tags))
 
-    def generate_user_and_profile(self):
+    def generate_user_and_profile(self, ratio):
         def generate_user(num):
             user_dict_repr = {
                 'username': f'User{num}',
@@ -36,24 +39,25 @@ class Command(BaseCommand):
                 'is_superuser': False
             }
             return user_dict_repr
+
         profiles = []
-        for i in range(self.GENERATION_ORDER):
+        for i in range(ratio):
             user = User.objects.create_user(**generate_user(i))
             p = Profile(user=user)
             profiles.append(p)
         Profile.objects.bulk_create(profiles)
 
-    def generate_tags(self):
+    def generate_tags(self, ratio):
         tags = []
-        for i in range(self.GENERATION_ORDER):
+        for i in range(ratio):
             t = Tags()
             t.name = f'tag{i}'
             tags.append(t)
         Tags.objects.bulk_create(tags)
 
-    def generate_questions(self, profiles):
+    def generate_questions(self, profiles, ratio):
         questions = []
-        for i in range(self.GENERATION_ORDER * 10):
+        for i in range(ratio * 10):
             author = random.choice(profiles)
             q = Questions()
             q.title = f'Question {i}'
@@ -62,9 +66,9 @@ class Command(BaseCommand):
             questions.append(q)
         Questions.objects.bulk_create(questions)
 
-    def generate_answers(self, profiles, questions):
+    def generate_answers(self, profiles, questions, ratio):
         answers = []
-        for i in range(self.GENERATION_ORDER * 100):
+        for i in range(ratio * 100):
             author = random.choice(profiles)
             a = Answers()
             a.text = f'answer {i}'
@@ -73,18 +77,18 @@ class Command(BaseCommand):
             answers.append(a)
         Answers.objects.bulk_create(answers)
 
-    def generate_question_likes(self, profiles, questions):
+    def generate_question_likes(self, profiles, questions, ratio):
         likes = []
-        for i in range(self.GENERATION_ORDER * 200):
+        for i in range(ratio * 200):
             author = random.choice(profiles)
             question = random.choice(questions)
             like = QuestionLike(user=author, question=question)
             likes.append(like)
         QuestionLike.objects.bulk_create(likes)
 
-    def generate_answer_likes(self, profiles, answers):
+    def generate_answer_likes(self, profiles, answers, ratio):
         likes = []
-        for i in range(self.GENERATION_ORDER * 200):
+        for i in range(ratio * 200):
             author = random.choice(profiles)
             answer = random.choice(answers)
             like = AnswerLike(user=author, answer=answer)
