405a5dadce7621d1aa4a92a3deeeb4ebde7836f6
Gallaann <reindzer26rus@gmail.com>
2023-09-26 21:23:01 +0300

    test

--- askme/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.Questions)
-admin.site.register(models.Answers)
-admin.site.register(models.Tags)
-admin.site.register(models.Profile)
-admin.site.register(models.AnswerLike)
-admin.site.register(models.QuestionLike)
